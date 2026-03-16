96574f8e3671effbff7b58ab33f602abb4b11170
JaneZ <jane@zdravevski.pro>
2026-03-16 22:46:28 +0100

    Added markdown render for blog posts.

--- frontend/components/blog/blog-content.tsx
+++ frontend/components/blog/blog-content.tsx
@@ -1,5 +1,8 @@
 'use client';
 
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import rehypeRaw from 'rehype-raw';
 import { cn } from '@/lib/utils';
 
 interface BlogContentProps {
@@ -9,12 +12,10 @@ interface BlogContentProps {
 
 export function BlogContent({ content, className }: BlogContentProps) {
   return (
-    <div
-      className={cn(
-        'prose-custom',
-        className
-      )}
-      dangerouslySetInnerHTML={{ __html: content }}
-    />
+    <div className={cn('prose-custom', className)}>
+      <ReactMarkdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]}>
+        {content}
+      </ReactMarkdown>
+    </div>
   );
 }
